509e9e4ef91447ae3a0f5e730a8539c99d14193e
gerpang <geraldinepyh@gmail.com>
2021-07-28 12:43:07 +0800

    backend to sqlite

---
+++ dbhelper.py
@@ -0,0 +1,65 @@
+import sqlite3
+
+class DBHelper:
+    def __init__(self, dbname="events.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname, check_same_thread=False)
+        self.setup()
+
+    def setup(self):
+        print("creating table")
+        stmt ="""CREATE TABLE IF NOT EXISTS events (
+                    event_id INTEGER PRIMARY KEY,
+                    event_name TEXT,
+                    event_date TEXT,
+                    event_time TEXT,
+                    duration INTEGER,
+                    duration_unit TEXT,
+                    attendees TEXT,
+                    venue TEXT,
+                    owner TEXT
+                );"""
+        self.conn.execute(stmt)
+        self.conn.commit()
+
+    def new_event_id(self):
+        stmt = "SELECT MAX(event_id) FROM events;"
+        return self.conn.execute(stmt).fetchone()[0] + 1;
+
+    def add_event(self, event_details, owner):
+        try:
+            # temporary: to ensure that all the details are given.
+            assert len(event_details) == 5
+        except AssertionError as e:
+            print("Missing information about the event")
+
+        stmt = "INSERT INTO events (event_id, event_name, event_date, event_time, attendees, venue, owner) VALUES (?, ?, ?, ?, ?, ?, ?)"
+        event_id = self.new_event_id()
+        print(event_id)
+        args = (event_id, *event_details, owner)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_event(self, event_id, owner):
+        stmt = "DELETE FROM events WHERE event_id = (?) AND owner = (?)"
+        args = (event_id, owner )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def get_events(self, owner):
+        stmt = "SELECT event_id, event_name FROM events WHERE owner = (?)"
+        args = (owner, )
+        return [x[0:2] for x in self.conn.execute(stmt, args)]
+    
+    def get_event_info(self, owner, event_id):
+        stmt = """
+        SELECT event_id, event_name, event_date, event_time, venue, attendees FROM events WHERE owner = (?) AND event_id = (?)
+        ;"""
+        args = (owner, event_id)
+        c = self.conn.cursor().execute(stmt, args)
+        cols = [x[0] for x in c.description]
+        details = c.fetchall()
+        event_dic = {c:d for c, d in zip(cols, details[0])}
+        return event_dic
+
+db = DBHelper()
--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    ConversationHandler,
+    MessageHandler,
+    Filters,
+)
 import logging
 from commands import *
 import json
 
+
 def main():
-    with open('token.json','r') as f:
-        telegram_token = json.load(f)["Telegram"]['token']
+
+    with open("token.json", "r") as f:
+        telegram_token = json.load(f)["Telegram"]["token"]
     updater = Updater(token=telegram_token)
-    dp = updater.dispatcher 
-    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                     level=logging.INFO)
+    dp = updater.dispatcher
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+    )
 
     # Add handlers for allowed commands
-    dp.add_handler(CommandHandler('start',start))
-    dp.add_handler(CommandHandler('bop',bop))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("bop", bop))
 
     create_events_request_handler = ConversationHandler(
         entry_points=[CommandHandler("create_event", create_event)],
         states={
             CREATE: [MessageHandler(Filters.text & (~Filters.command), create_event_gs)]
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
     dp.add_handler(create_events_request_handler)
-
 
     list_events_request_handler = ConversationHandler(
         entry_points=[CommandHandler("list_events", list_events)],
         states={
-            LIST: [MessageHandler(Filters.text & (~Filters.command), list_events_requested)]
+            LIST: [
+                MessageHandler(Filters.text & (~Filters.command), list_events_requested)
+            ]
             # GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
             # PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
             # LOCATION: [
@@ -37,13 +48,14 @@ def main():
             # ],
             # BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
     dp.add_handler(list_events_request_handler)
 
-    dp.add_handler(MessageHandler(Filters.command, unknown)) # must be last
+    dp.add_handler(MessageHandler(Filters.command, unknown))  # must be last
     updater.start_polling()
     updater.idle()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
